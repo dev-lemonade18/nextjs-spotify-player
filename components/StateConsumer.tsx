3b246970f800d980916a5a61f7e82e83cc363538
dev-lemonade18 <dev.lemonade18@gmail.com>
2021-08-03 07:43:24 +0900

    try

--- components/StateConsumer.tsx
+++ components/StateConsumer.tsx
@@ -30,18 +30,40 @@ export const StateConsumer: React.VFC<{ access_token: string }> = memo(
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [playerDevice?.device_id]);
 
-    useEffect(() => {
-      if (playbackState?.track_window.current_track === undefined) return;
-      console.log(`https://spclient.wg.spotify.com/color-lyrics/v1/track/${playbackState?.track_window.current_track.id}/image/${encodeURIComponent(playbackState?.track_window.current_track.album.images[2].url)}?market=from_token`)
-      fetch(`https://spclient.wg.spotify.com/color-lyrics/v1/track/${playbackState?.track_window.current_track.id}/image/${encodeURIComponent(playbackState?.track_window.current_track.album.images[2].url)}?market=from_token`, {
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(async () => {
+      const res = await fetch("https://open.spotify.com/get_access_token?reason=transport&productType=web_player", {
         "headers": {
           "accept": "application/json",
           "accept-language": "ja",
           "app-platform": "WebPlayer",
-          "authorization": "Bearer " + access_token,
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+          "spotify-app-version": "1.1.54.35.ge9dace1d"
+        },
+        "referrer": "https://open.spotify.com/lyrics",
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": null,
+        "method": "GET",
+        "mode": "cors"
+      }).then((r) => r.json());
+      const token = res.accessToken;
+
+      if (playbackState?.track_window.current_track.id === undefined) return;
+      if (playbackState?.track_window.current_track.album.images[0].url === undefined) return;
+      const lyricJson = await fetch(`https://spclient.wg.spotify.com/color-lyrics/v1/track/${playbackState?.track_window.current_track.id}/image/${encodeURIComponent(playbackState?.track_window.current_track.album.images[0].url)}?market=from_token`, {
+        "headers": {
+          "accept": "application/json",
+          "accept-language": "ja",
+          "app-platform": "WebPlayer",
+          "authorization": "Bearer " + token,
           "sec-ch-ua": "\"Chromium\";v=\"92\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"92\"",
           "sec-ch-ua-mobile": "?0",
-          "spotify-app-version": "1.1.66.47.g3afed57a"
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+          "spotify-app-version": "1.1.66.50.g895aca06"
         },
         "referrer": "https://open.spotify.com/",
         "referrerPolicy": "strict-origin-when-cross-origin",
@@ -49,7 +71,7 @@ export const StateConsumer: React.VFC<{ access_token: string }> = memo(
         "method": "GET",
         "mode": "cors"
       });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+      console.log(lyricJson);
     }, [playbackState?.track_window]);
 
     return (
