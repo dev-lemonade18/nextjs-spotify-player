57376e9ff11bdcaaf11d58f64be19a81aaa82549
dev-lemonade18 <dev.lemonade18@gmail.com>
2021-08-07 11:32:24 +0900

    fixed bug

--- components/LyricsView.tsx
+++ components/LyricsView.tsx
@@ -20,12 +20,9 @@ export const LyricsView: React.VFC<Props> = memo(({ current_time, lyrics_object 
     const [open, setOpen] = useState<boolean>(false);
     var lyric = ""
     if (lyrics_object !== undefined && current_time !== undefined && current_time !== null) {
-        var current_index = 0
         for (var index = 0; index < Object.keys(lyrics_object).length; index++) {
             if (lyrics_object[index].starttime <= current_time + 500 && current_time + 500 >= lyrics_object[index].endtime) {
                 lyric = lyrics_object[index].line
-            } else {
-                lyric = ""
             }
         }
     }
